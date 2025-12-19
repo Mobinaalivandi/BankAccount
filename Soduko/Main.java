5a1b0465205d7b874bb7d17e903939ceed872807
Mobinaalivandi <mobinavafa2006@gmail.com>
2025-12-19 09:34:37 -0800

    Update Main.java

--- Soduko/Main.java
+++ Soduko/Main.java
@@ -20,87 +20,71 @@ public class Main {
             for (int j = 0; j < 4; ++j) {
                 if (sud[i][j] <= 0 || sud[i][j] >= 5) {
                     find = false;
-                        }
-                    }
                 }
+            }
+        }
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
                 for (int k = j + 1; k < 4; ++k) {
                     if (sud[i][j] == sud[i][k]) {
                         find = false;
-                            }
-                        }
                     }
                 }
+            }
+        }
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
                 for (int k = j + 1; k < 4; ++k) {
                     if (sud[j][i] == sud[k][i]) {
                         find = false;
-                            }
-                        }
                     }
                 }
+            }
+        }
         for (int i = 0; i < 2; ++i) {
             for (int j = 0; j < 2; ++j) {
                 for (int p = i + 1; p < 2; ++p) {
                     for (int k = j + 1; k < 2; ++k) {
                         if (sud[i][j] == sud[p][k]) {
                             find = false;
-                        } else if (sud[i][j] == sud[i][k]) {
-                            find = false;
-                        } else if (sud[i][j] == sud[p][j]) {
-                            find = false;
-                                }
-                            }
                         }
                     }
                 }
+            }
+        }
         for (int i = 0; i < 2; ++i) {
             for (int j = 2; j < 4; ++j) {
                 for (int p = i + 1; p < 2; ++p) {
                     for (int k = j + 1; k < 4; ++k) {
                         if (sud[i][j] == sud[p][k]) {
                             find = false;
-                        } else if (sud[i][j] == sud[i][k]) {
-                            find = false;
-                        } else if (sud[i][j] == sud[p][j]) {
-                            find = false;
-                                }
-                            }
                         }
                     }
                 }
+            }
+        }
         for (int i = 2; i < 4; ++i) {
             for (int j = 0; j < 2; ++j) {
                 for (int p = i + 1; p < 4; ++p) {
                     for (int k = j + 1; k < 2; ++k) {
                         if (sud[i][j] == sud[p][k]) {
                             find = false;
-                        } else if (sud[i][j] == sud[i][k]) {
-                            find = false;
-                        } else if (sud[i][j] == sud[p][j]) {
-                            find = false;
-                                }
-                            }
                         }
                     }
                 }
+            }
+        }
         for (int i = 2; i < 4; ++i) {
             for (int j = 2; j < 4; ++j) {
                 for (int p = i + 1; p < 4; ++p) {
                     for (int k = j + 1; k < 4; ++k) {
                         if (sud[i][j] == sud[p][k]) {
                             find = false;
-                        } else if (sud[i][j] == sud[i][k]) {
-                            find = false;
-                        } else if (sud[i][j] == sud[p][j]) {
-                            find = false;
-                                }
-                            }
                         }
                     }
                 }
+            }
+        }
         if (find) {
             System.out.println("This soduko is valid");
         } else {
@@ -108,5 +92,7 @@ public class Main {
                 }
             }
         }
+
+
 
 
